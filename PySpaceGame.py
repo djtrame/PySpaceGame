4fbe64b985dd7d8dc871981c58b978ad7f1e328e
David Trame <king_bucket@hotmail.com>
2017-02-03 22:40:06 -0500

    I have a row of alien ships, the missle can hit them and they both disappear.  Also have the aliens doing stutter stepping.

--- PySpaceGame.py
+++ PySpaceGame.py
@@ -22,9 +22,31 @@ def main():
     gameOver = False
     gameExit = False
 
-    mySpaceShip = HumanSpaceShip(500, 500, 20, 20, c.blue)
+    mySpaceShip = HumanSpaceShip(500, 500, 30, 30, c.blue)
+    alienSpaceShipSize = 30
+    alienFirstRowX = 120
+    alienFirstRowY = 200
+    alienStutterStepDistance = -1
+    alienStutterStepFrameCounter = 0
+    alienStutterStepSpeed = 60 #the lower the number = the faster the aliens step left and right
+    alienSpaceShips = []
+
+    lastFire = 0
+    missleOffCooldown = True
+
+
+
+    #create alien ships
+    for i in range(0,10):
+        newAlienSpaceShip = AlienSpaceShip1(alienFirstRowX + (i * alienSpaceShipSize * 2), alienFirstRowY, alienSpaceShipSize, alienSpaceShipSize, c.gray)
+        alienSpaceShips.append(newAlienSpaceShip)
 
     while not gameExit:
+        #each frame check to see if the human missle is able to fire
+        if not missleOffCooldown:
+            now = pygame.time.get_ticks()
+            if (now - lastFire) > mySpaceShip.missleCooldown:
+                missleOffCooldown = True
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -46,7 +68,13 @@ def main():
 
                 elif event.key == pygame.K_SPACE:
                     print('fire')
-                    mySpaceShip.fireMissle(gameDisplay)
+
+                    #each frame we check if the missle is off cooldown.
+                    #the default is True so this will fire the first time no matter what
+                    if missleOffCooldown:
+                        mySpaceShip.fireMissle(gameDisplay)
+                        missleOffCooldown = False
+                        lastFire = pygame.time.get_ticks()
 
                 elif event.key == pygame.K_w:
                     print('pass')
@@ -89,16 +117,44 @@ def main():
 
         gameDisplay.fill(c.black)
 
-        mySpaceShip.x += mySpaceShip.x_change
+        #if missles have been fired move them by 1 frame, check if they went off the screen, and check if they collided with something
+        if len(mySpaceShip.missles) > 0:
+            for missle in mySpaceShip.missles:
+                missle.move()
+                if not missle.check_bounds(display_width, display_height):
+                    print('missle out of bounds')
+                    mySpaceShip.missles.remove(missle)
+                else:
+                    #check collisions with all alien ships
+                    if len(alienSpaceShips) > 0:
+                        for alienSS in alienSpaceShips:
+                            if missle.check_collision(alienSS):
+                                mySpaceShip.missles.remove(missle)
+                                alienSpaceShips.remove(alienSS)
+
+                pygame.draw.rect(gameDisplay, missle.color, (missle.x, missle.y, missle.width, missle.height))
+
+        mySpaceShip.move()
 
         pygame.draw.rect(gameDisplay, mySpaceShip.color, (mySpaceShip.x, mySpaceShip.y, mySpaceShip.width, mySpaceShip.height))
 
-        if len(mySpaceShip.missles) > 0:
-            missle = mySpaceShip.missles[0]
-            missle.move()
-            pygame.draw.rect(gameDisplay, missle.color, (missle.x, missle.y, missle.width, missle.height))
+        if len(alienSpaceShips) > 0:
+            #decide if the aliens are stutter stepping right or left
+            stutterStep = 'right'
+            alienStutterStepFrameCounter += 1
+            if alienStutterStepFrameCounter > alienStutterStepSpeed:
+                alienStutterStepFrameCounter = 0
+                alienStutterStepDistance *= -1
+                if stutterStep == 'right':
+                    stutterStep == 'left'
+                else:
+                    stutterStep == 'right'
 
-        pygame.draw.rect(gameDisplay, c.green, (400, 300, 10, 10))
+
+            for alienSS in alienSpaceShips:
+                alienSS.x_change = alienStutterStepDistance
+                alienSS.move()
+                pygame.draw.rect(gameDisplay, alienSS.color, (alienSS.x, alienSS.y, alienSS.width, alienSS.height))
 
         pygame.display.update()
         clock.tick(FPS)
--- PySpaceGame_Classes.py
+++ PySpaceGame_Classes.py
@@ -5,38 +5,55 @@ import colors as c
 class SpaceShip:
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, x, y, width, height, color):
+    def __init__(self, x, y, width, height, color, missleCooldown):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.color = color
+        self.missleCooldown = missleCooldown
 
         #this is the amount of pixels this object moves per frame
         self.x_change = 0
         self.y_change = 0
         self.missles = []
 
+    def leftSide(self):
+        return self.x
+
+    def rightSide(self):
+        return self.x + self.width
+
+    def topSide(self):
+        return self.y
+
+    def bottomSide(self):
+        return self.y + self.height
+
+    def move(self):
+        self.x += self.x_change
+
     @abc.abstractmethod
     def fireMissle(self, gameDisplay):
         """Implement a fireMissle method for each type of SpaceShip"""
         return
 
+
 class HumanSpaceShip(SpaceShip):
-    def __init__(self, x, y, width, height, color):
-        super().__init__(x, y, width, height, color)
+    def __init__(self, x, y, width, height, color, missleCooldown=500):
+        super().__init__(x, y, width, height, color, missleCooldown)
         self.type = 'human'
         self.missleColor = c.red
 
     #now handle removing the missle from the collection when it hits the boundary of the screen
     def fireMissle(self, gameDisplay):
-        newMissle = Missle(self.x, self.y, 5, 20, self.missleColor)
+        newMissle = Missle(self.x + (self.width / 2), self.y, 5, 20, self.missleColor)
         self.missles.append(newMissle)
 
 
 class AlienSpaceShip1(SpaceShip):
-    def __init__(self, x, y, width, height, color):
-        super().__init__(x, y, width, height, color)
+    def __init__(self, x, y, width, height, color, missleCooldown=500):
+        super().__init__(x, y, width, height, color, missleCooldown)
         self.type = 'alien'
 
     def fireMissle(self, gameDisplay):
@@ -58,7 +75,41 @@ class Missle():
         #defaulting the y movement to 5 so its default behavior is to fly
         self.y_change = -5
 
+    def leftSide(self):
+        return self.x
+
+    def rightSide(self):
+        return self.x + self.width
+
+    def topSide(self):
+        return self.y
+
+    def bottomSide(self):
+        return self.y + self.height
+
     #@abc.abstractmethod
     def move(self):
         self.x += self.x_change
         self.y += self.y_change
+
+    #returns True if the missle goes off the screen
+    def check_bounds(self, display_width, display_height):
+        if 0 < self.x < display_width and 0 < self.y < display_height:
+            return True
+        else:
+            return False
+
+    #returns True if the missle has collided with the spaceShip
+    def check_collision(self, spaceShip):
+        #if the missle crosses any boundary of the space ship, return true and we'll blow up the missle and the space ship
+        if (spaceShip.leftSide() < self.leftSide() < spaceShip.rightSide()) or \
+                (spaceShip.leftSide() < self.rightSide() < spaceShip.rightSide()):
+            #print('missle within X bounds of spaceship')
+            if (spaceShip.topSide() < self.topSide() < spaceShip.bottomSide()) or \
+                    (spaceShip.topSide() < self.bottomSide() < spaceShip.bottomSide()):
+                #print ('missle within x and y bounds of spaceship')
+                return True
+        else:
+            return False
+
+
